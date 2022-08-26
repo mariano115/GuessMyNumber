90dbe676ecb3ea0653fc5ec12f6bc5db8ed16352
Mariano <marianogermanguerrero@gmail.com>
2022-08-26 11:44:39 -0300

    useWindowDimensions

--- components/game/NumberContainer.js
+++ components/game/NumberContainer.js
@@ -1,23 +1,32 @@
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Dimensions,
+  useWindowDimensions,
+} from "react-native";
 
 import Colors from "../../constants/colors";
 
 export default function NumberContainer({ children }) {
+  const { width, height } = useWindowDimensions();
+
+  const padding = width > 450 ? 12 : 24;
+  const margin = width > 450 ? 12 : 24;
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, { padding, margin }]}>
       <Text style={styles.numberText}>{children}</Text>
     </View>
   );
 }
 
-const deviceWidth = Dimensions.get("window").width;
-
 const styles = StyleSheet.create({
   container: {
     borderWidth: 4,
     borderColor: Colors.accent500,
-    padding: deviceWidth < 380 ? 12 : 24,
-    margin: deviceWidth < 380 ? 12 : 24,
+    /* padding: deviceWidth > 450 ? 12 : 24,
+    margin: deviceWidth > 450 ? 12 : 24, */
     borderRadius: 8,
     alignItems: "center",
     justifyContent: "center",
